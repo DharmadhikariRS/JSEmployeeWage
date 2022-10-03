b918f85a59d621302435bfa97c265aa30e32123a
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-10-03 23:38:10 +0530

    Employee wage parttime fulltime

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,7 +4,43 @@ function CheckPresentAbsent() {
 let empCheck = CheckPresentAbsent();
 const isFullTime = 1;
 if (empCheck == isFullTime) {
-  console.log("Employee is present");
+  console.log("UC1- Employee is present");
 } else {
-  console.log("Employee is Absent", empCheck);
+  console.log("UC1- Employee is Absent", empCheck);
 }
+//--------------------------------------------------
+function CheckFullPartTime() {
+    return Math.floor((Math.random() * 10) % 3);
+  }
+  
+  empCheck = CheckFullPartTime();
+  const isPartTime = 2;
+  const wagePerHour = 20;
+  let empHrs;
+  let dailyWage;
+  
+  switch (empCheck) {
+    case isFullTime:
+      empHrs = 8;
+      dailyWage = wagePerHour * empHrs;
+  
+      console.log(
+        "UC2- Employee is present for full time and his salary is: " + dailyWage
+      );
+      break;
+  
+    case isPartTime:
+      empHrs = 4;
+      dailyWage = wagePerHour * empHrs;
+  
+      console.log(
+        "UC2- Employee is present for Part time and his salary is: " + dailyWage
+      );
+      break;
+  
+    default:
+      empHrs = 0;
+      dailyWage = wagePerHour * empHrs;
+      console.log("UC2- Employee is absent and his salary is: " + dailyWage);
+      break;
+  }
