62a5984087b05114bc3a5d93611ae1a8e31197ea
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-10-04 23:06:56 +0530

    Store daily wages

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -54,11 +54,11 @@ function GetWorkHrs(a){
 }
 console.log("UC-3 WorkHrs= "+WorkHrs)
 
-//-----------------------------------
+//-------------------------------------------------------------
   let TotalWage=0;
   let NumOfDays=20;
   let Day=1;
-
+let DailyWageArray=[]
 function GetEmpHrs() {
     empCheck=CheckFullPartTime() 
     switch (empCheck) {
@@ -72,29 +72,36 @@ function GetEmpHrs() {
         empHrs = 0;
         break;
     }
+   
     return empHrs;
   }
+ /* 
 let Workhr=0;
   while(Day<=NumOfDays){
-    Workhr+=GetEmpHrs()
+   
   Day++
 }
 TotalWage=(Workhr*wagePerHour)
 console.log("UC4-WorkHrs & TotalWage= "+Workhr+" and "+TotalWage)
-
+*/
+//-------------------------------------------------------------------------
 let MaxWorkHrs=160;
 let MaxWorkDays=20;
 Day=0;
-WorkHrs=0;
+WorkHr=0;
 let a=0;
 while(WorkHrs<=MaxWorkHrs && Day<MaxWorkDays){
     a=GetEmpHrs()
-    WorkHrs+=a
+    if(WorkHr>MaxWorkHrs){
+      WorkHr-=a;
+       break;
+        }
+    DailyWageArray.push(a*wagePerHour)
+    WorkHr+=a
     Day++
-  if(Workhr>MaxWorkHrs){
-WorkHrs-=a;
- break;
-  }
+
 }
-TotalWage=(Workhr*wagePerHour);
-console.log("UC5 Calculating Workhrs, Days and TotalWage for Max 20 days or 160 hrs condition= "+Workhr+","+Day+" and "+TotalWage)
+TotalWage=(WorkHr*wagePerHour);
+console.log("UC5 TotalworkHrs= "+WorkHr+" "+ "Totaldays= "+Day+ " "+ "TotalWage= "+TotalWage)
+//--------------------------------------------------------------------
+console.log("UC6-Daily Wages are: "+DailyWageArray.toString())
